2eef72dc63fb69223f1b0daf3a7dec8a3cd52c8c
Wes Matlock <thomasmatlockbba@gmail.com>
2023-11-14 20:35:33 -0600

    Add fooditems

---
+++ Flush_It_API/Controllers/FoodController.cs
@@ -0,0 +1,48 @@
+﻿using Flush_It_API.Data;
+using Flush_It_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flush_It_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public FoodController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetFoodItems()
+        {
+            return Ok(_context.Food.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFoodItem(int id)
+        {
+            var food = await _context.Food.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            return Ok(food);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateFoodItem([FromBody] Food food)
+        {
+            if (food == null) return BadRequest();
+
+            _context.Food.Add(food);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetFoodItem), new { id = food.Id }, food);
+        }
+    }
+}
---
+++ Flush_It_API/Migrations/20231115020450_Food.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Flush_It_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Food : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Food",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Calories = table.Column<int>(type: "integer", nullable: true),
+                    Sugar = table.Column<double>(type: "double precision", nullable: true),
+                    Sodium = table.Column<double>(type: "double precision", nullable: true),
+                    Potassium = table.Column<double>(type: "double precision", nullable: true),
+                    Calcium = table.Column<double>(type: "double precision", nullable: true),
+                    Iron = table.Column<double>(type: "double precision", nullable: true),
+                    Carbs = table.Column<double>(type: "double precision", nullable: true),
+                    Protein = table.Column<double>(type: "double precision", nullable: true),
+                    TotalFat = table.Column<double>(type: "double precision", nullable: true),
+                    SaturatedFat = table.Column<double>(type: "double precision", nullable: true),
+                    TransFat = table.Column<double>(type: "double precision", nullable: true),
+                    Cholesterol = table.Column<double>(type: "double precision", nullable: true),
+                    Fiber = table.Column<double>(type: "double precision", nullable: true),
+                    Alcohol = table.Column<bool>(type: "boolean", nullable: true),
+                    Processed = table.Column<bool>(type: "boolean", nullable: true),
+                    FODMAP = table.Column<bool>(type: "boolean", nullable: true),
+                    DairyProduct = table.Column<bool>(type: "boolean", nullable: true),
+                    Gluten = table.Column<bool>(type: "boolean", nullable: true),
+                    Spicy = table.Column<bool>(type: "boolean", nullable: true),
+                    Fried = table.Column<bool>(type: "boolean", nullable: true),
+                    Caffeine = table.Column<bool>(type: "boolean", nullable: true),
+                    Sweetener = table.Column<bool>(type: "boolean", nullable: true),
+                    Bread = table.Column<bool>(type: "boolean", nullable: true),
+                    ArtificialAdditives = table.Column<bool>(type: "boolean", nullable: true),
+                    CarbonatedBeverage = table.Column<bool>(type: "boolean", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Food", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Food");
+        }
+    }
+}
